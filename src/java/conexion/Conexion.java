0eab2fbf836eeca96c124f08b67ccd78a14a86f7
DAVMARROS <deivid.derm1996@gmail.com>
2019-11-12 13:33:48 -0600

    Web services

--- src/java/conexion/Conexion.java
+++ src/java/conexion/Conexion.java
@@ -15,9 +15,9 @@ public class Conexion {
     public DriverManagerDataSource Conectar(){
         DriverManagerDataSource nav = new DriverManagerDataSource();
         nav.setDriverClassName("com.mysql.jdbc.Driver");
-        nav.setUrl("jdbc:mysql://localhost:3306/MSC2019");
+        nav.setUrl("jdbc:mysql://node49314-empleados.jl.serv.net.mx:3306/MSC2019");
         nav.setUsername("root");
-        nav.setPassword("");
+        nav.setPassword("XVOrvo39422");
         return nav;
     }
 }
--- src/java/controller/EmpleadosContrador.java
+++ src/java/controller/EmpleadosContrador.java
@@ -6,12 +6,15 @@
 package controller;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonParser;
+import com.sun.xml.rpc.processor.modeler.rmi.JavaBean;
 import conexion.Conexion;
 import java.util.ArrayList;
 import java.util.List;
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -51,45 +54,41 @@ public class EmpleadosContrador {
     
     @RequestMapping(value="postEmpleado.htm", produces = "application/json", method=RequestMethod.POST)
     @ResponseBody
-    public String postEmpleado(@RequestBody String data){
-        com.google.gson.JsonObject jsonObject = new JsonParser().parse(data).getAsJsonObject();
-        String nombre = String.valueOf(jsonObject.get("nombre"));
-        String sueldo = String.valueOf(jsonObject.get("sueldo"));
+    public String postEmpleado(HttpServletRequest Request){
+        String nombre = String.valueOf(Request.getParameter("nombre"));
+        String sueldo = String.valueOf(Request.getParameter("sueldo"));
         nombre = nombre.replace("\"", "");
         String sql = "insert into empleados (nombre,sueldo) values('"+nombre+"','"+sueldo+"');";
         this.jdbctemplate.execute(sql);
-
-        // Ejecuta petición sql
-        String json = new Gson().toJson("Success");
+        com.google.gson.JsonObject datos = new JsonParser().parse("{\"message\": \"success\"}").getAsJsonObject();
+        String json = new Gson().toJson(datos);
         return json;
     }
     
     @RequestMapping(value="putEmpleado.htm", produces = "application/json", method=RequestMethod.POST)
     @ResponseBody
-    public String putEmpleado(@RequestBody String data){
-        com.google.gson.JsonObject jsonObject = new JsonParser().parse(data).getAsJsonObject();
-        String clave = String.valueOf(jsonObject.get("clave"));
-        String nombre = String.valueOf(jsonObject.get("nombre"));
-        String sueldo = String.valueOf(jsonObject.get("sueldo"));
+    public String putEmpleado(HttpServletRequest Request){
+        //com.google.gson.JsonObject jsonObject = new JsonParser().parse(data).getAsJsonObject();
+        String clave = String.valueOf(Request.getParameter("clave"));
+        String nombre = String.valueOf(Request.getParameter("nombre"));
+        String sueldo = String.valueOf(Request.getParameter("sueldo"));
         nombre = nombre.replace("\"", "");
         String sql = "update empleados set nombre='"+nombre+"', sueldo='"+sueldo+"' where clave='"+clave+"';";
         this.jdbctemplate.execute(sql);
-
-        // Ejecuta petición sql
-        String json = new Gson().toJson("Success");
+        com.google.gson.JsonObject datos = new JsonParser().parse("{\"message\": \"success\"}").getAsJsonObject();
+        String json = new Gson().toJson(datos);
         return json;
     }
     
     @RequestMapping(value="deleteEmpleado.htm", produces = "application/json", method=RequestMethod.POST)
     @ResponseBody
-    public String deleteEmpleado(@RequestBody String data){
-        com.google.gson.JsonObject jsonObject = new JsonParser().parse(data).getAsJsonObject();
-        String clave = String.valueOf(jsonObject.get("clave"));
+    public String deleteEmpleado(HttpServletRequest Request){
+        //com.google.gson.JsonObject jsonObject = new JsonParser().parse(data).getAsJsonObject();
+        String clave = String.valueOf(Request.getParameter("clave"));
         String sql = "delete from empleados where clave='"+clave+"';";
         this.jdbctemplate.execute(sql);
-
-        // Ejecuta petición sql
-        String json = new Gson().toJson("Success");
+        com.google.gson.JsonObject datos = new JsonParser().parse("{\"message\": \"success\"}").getAsJsonObject();
+        String json = new Gson().toJson(datos);
         return json;
     }
     
